110acf00234791955de57347cd48d9df4b5927a2
Stefan Bley <stefan.bley@saxsys.de>
2014-01-18 01:31:45 +0100

    read project names from gui task

--- javafx/src/main/java/de/saxsys/projectiler/ProjectilerTask.java
+++ javafx/src/main/java/de/saxsys/projectiler/ProjectilerTask.java
@@ -6,16 +6,16 @@ import de.saxsys.projectiler.selenium.Settings;
 
 public class ProjectilerTask extends Task<Boolean> {
 
-    @Override
-    protected Boolean call() throws Exception {
-        try {
-            final Projectiler projectiler = new Projectiler(new SeleniumCrawler(new Settings()));
-            projectiler.clock();
-        } catch (final Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+	@Override
+	protected Boolean call() throws Exception {
+		try {
+			final Projectiler projectiler = new Projectiler(new SeleniumCrawler(new Settings()));
+			System.out.println("Deine Projekte: " + projectiler.getProjectNames());
+		} catch (final Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 
 }
