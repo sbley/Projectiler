b09abc4efaacd5606e8faf3fec486079944af5d7
spaxx86 <svbascript86>
2014-05-27 11:08:22 +0200

    only upload tracks from today

--- android/Projectiler/app/src/main/java/de/saxsys/android/projectiler/app/db/DataProvider.java
+++ android/Projectiler/app/src/main/java/de/saxsys/android/projectiler/app/db/DataProvider.java
@@ -3,6 +3,9 @@ package de.saxsys.android.projectiler.app.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import de.saxsys.android.projectiler.app.generatedmodel.DaoMaster;
@@ -38,16 +41,49 @@ public class DataProvider {
 
     }
 
-    public void saveTrack(final Track track){
+    public void saveTrack(final Track track) {
         trackDao.insertOrReplace(track);
     }
 
-    public void deleteTrack(final Track track){
+    public void deleteTrack(final Track track) {
         trackDao.delete(track);
     }
 
-    public List<Track> getTracks(){
-        return trackDao.loadAll();
+    public List<Track> getTracks() {
+        List<Track> tracks = trackDao.loadAll();
+        List<Track> ret = new ArrayList<Track>();
+
+        for (Track track : tracks) {
+
+            if (isToday(track.getTimestamp())) {
+                ret.add(track);
+            }
+        }
+        return ret;
+    }
+
+    private boolean isToday(Date timestamp) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(timestamp);
+
+        Calendar calToday = Calendar.getInstance();
+        calToday.setTime(new Date());
+
+        int dayToday = calToday.get(Calendar.DAY_OF_MONTH);
+        int monthToday = calToday.get(Calendar.MONTH);
+        int yearToday = calToday.get(Calendar.YEAR);
+
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
+
+
+        if (day == dayToday && month == monthToday && year == yearToday) {
+            return true;
+        }
+
+        return false;
     }
 
 }
